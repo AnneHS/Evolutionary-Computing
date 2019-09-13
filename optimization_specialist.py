8a88466a33e2b8c0cf3063ab2a7c91ad98616e17
lennoir <49302751+lennoir@users.noreply.github.com>
2019-09-13 13:26:09 +0200

    Add files via upload

--- optimization_specialist.py
+++ optimization_specialist.py
@@ -5,6 +5,22 @@
 # lennart.frahm@web.de        				                                  #
 ###############################################################################
 
+"""
+Things to Improve/Think about it:
+- Survival selection (possibly roulette wheel)
+- think about how neural nets relate to output -> connected to crossover (does it make sense to average parents?)
+    - does it make any sense to do crossover? is it not best to just try as many random solution as possible?
+    - different kind of inheritance?
+- think about fitness function, very small difference with solutions that manage to kill enemy
+- evolutionary parameters (offspring, N, generations, mutation rate)
+- how to do doomsday?
+- maybe prefer shooting over other actions? possibly just bias the initial population -> less time till convergence
+- incest prevention
+- multi-parent reproduction
+- mutation as a slight deviation of weight, not completely random
+- maybe sometimes kick out equal solutions?
+"""
+
 # imports framework
 import sys
 sys.path.insert(0, 'evoman')
@@ -18,12 +34,12 @@ import glob, os
 from random import randint
 
 # set up experiment folder
-experiment_name = '1209_1252'
+experiment_name = '1309_1153'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 env = Environment(experiment_name=experiment_name,
-                  enemies=[2],
+                  enemies=[3],
                   playermode="ai",
                   player_controller=player_controller(),
                   enemymode="static",
@@ -42,12 +58,11 @@ n_hidden = 10
 n_vars = (env.get_num_sensors()+1)*n_hidden + (n_hidden+1)*5 # multilayer with 10 hidden neurons
 upper_limit = 1
 lower_limit = -1
-
 N = 100
-gens = 30
-mutation_rate = 0.05
+gens = 20
+mutation_rate = 0.1
 k = 2
-number_of_offspring = np.random.randint(1,4)
+number_of_offspring = 2
 
 
 def documentation(pop, pop_fitness, i):
@@ -146,9 +161,6 @@ def initialization():
         print('\nNEW EVOLUTION\n')
         pop = np.random.uniform(lower_limit, upper_limit, (N, n_vars))
         pop_fitness = pop_evaluation(pop)
-        best = np.argmax(pop_fitness)
-        mean = np.mean(pop_fitness)
-        std = np.std(pop_fitness)
         ini_g = 0
         solutions = [pop, pop_fitness]
         env.update_solutions(solutions)
@@ -159,14 +171,14 @@ def initialization():
         pop = env.solutions[0]
         pop_fitness = env.solutions[1]
 
-        best = np.argmax(pop_fitness)
-        mean = np.mean(pop_fitness)
-        std = np.std(pop_fitness)
-
         # finds last generation number
         file_aux = open(experiment_name + '/gen.txt', 'r')
         ini_g = int(file_aux.readline())
         file_aux.close()
+
+    best = np.argmax(pop_fitness)
+    mean = np.mean(pop_fitness)
+    std = np.std(pop_fitness)
 
     file_aux = open(experiment_name + '/results.txt', 'a')
     file_aux.write('\n\ngen best mean std')
@@ -175,20 +187,62 @@ def initialization():
     file_aux.write('\n' + str(ini_g) + ' ' + str(round(pop_fitness[best], 6))
                    + ' ' + str(round(mean, 6)) + ' ' + str(round(std, 6)))
     file_aux.close()
-    return pop, pop_fitness, ini_g
+    return pop, pop_fitness, ini_g, best
 
 
 def evolution():
     ini = time.time()  # sets time marker
-    pop, pop_fitness, ini_g = initialization()
+    pop, pop_fitness, ini_g, best = initialization()
+    last_solution = pop_fitness[best]
+    not_improved = 0
     for i in range(ini_g + 1, gens):
         merged = survivor_selection(pop, pop_fitness)
-        pop = merged[:,:-1]
-        pop_fitness = merged[:,-1]
+        pop = merged[:, :-1]
+        pop_fitness = merged[:, -1]
         documentation(pop, pop_fitness, i)
+        best = np.argmax(pop_fitness)
+        if last_solution == pop_fitness[best]:
+            not_improved += 1
+            print(not_improved)
+        else:
+            not_improved = 0
+        if not_improved == 10:
+            pop, pop_fitness = doomsday(pop)
+            not_improved = 0
+        last_solution = pop_fitness[best]
+
     fim = time.time()  # prints total execution time for experiment
     print('\nExecution time: ' + str(round((fim - ini) / 60)) + ' minutes \n')
 
 
-evolution()
+def doomsday(pop):
+    # documentation
+    file_aux = open(experiment_name + '/results.txt', 'a')
+    file_aux.write('\ndoomsday')
+    file_aux.close()
+
+    # kill half the population, replace with random new solutions
+    survivors = pop[np.random.choice(N, int(N/2))]  # Potentially could be replaced by np.argsort(-pop_fitness)[0:(N/2)]
+    new_pop = np.random.uniform(lower_limit, upper_limit, (int(N/2), n_vars))
+    total_pop = np.r_[survivors, new_pop]
+    total_pop_fitness = pop_evaluation(total_pop)
+    return total_pop, total_pop_fitness
+
+if run_mode =='test':
+
+    bsol = np.loadtxt(experiment_name+'/best.txt')
+    print( '\n RUNNING SAVED BEST SOLUTION \n')
+    env.update_parameter('speed','normal')
+    fitness(np.array(bsol))
+    sys.exit(0)
+if run_mode =="train":
+    evolution()
 env.state_to_log() # checks environment state
+
+
+
+
+
+
+
+
