147e6082f7fcbfe5e23c5eb72b0a4b8d74c1308a
RicardoKnauer <40441748+RicardoKnauer@users.noreply.github.com>
2019-10-04 19:37:40 +0200

    Add files via upload

--- boxplots.py
+++ boxplots.py
@@ -1,7 +1,9 @@
 import sys
+
 sys.path.insert(0, "evoman")
 
 import os
+
 # avoid print statements for SPOT
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 # do not display pygame window
@@ -12,21 +14,23 @@ from evoman.environment import Environment
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
-plt.style.use("ggplot")
 
+plt.style.use("ggplot")
 
 RUNS = 10
 experiment_name = "halloffame"
 
+
 def fitness(single_weight_matrix):
-    f,e,p,t = env.play(pcont=single_weight_matrix)
+    f, e, p, t = env.play(pcont=single_weight_matrix)
     return f
+
 
 if __name__ == "__main__":
 
-    methods = ["DEAP","EA"]
-    enemies = [3,7,8]
-    best_fit = np.zeros((10,6))
+    methods = ["DEAP", "EA"]
+    enemies = [3, 7, 8]
+    best_fit = np.zeros((10, 6))
 
     for method_idx, method in enumerate(methods):
         for enemy_idx, enemy in enumerate(enemies):
@@ -40,16 +44,16 @@ if __name__ == "__main__":
                               logs="off")
 
             for run in range(RUNS):
-                best_contr = np.loadtxt("./{}/best_{}/run_{}.txt".format(method, enemy, run+1))
+                best_contr = np.loadtxt("./{}/best_{}/run_{}.txt".format(method, enemy, run + 1))
                 fit = float(fitness(best_contr))
                 if method_idx == 0:
-                    best_fit[run][method_idx+enemy_idx] = fit
+                    best_fit[run][method_idx + enemy_idx] = fit
                 elif method_idx == 1:
                     best_fit[run][2 + method_idx + enemy_idx] = fit
 
-    hof = pd.DataFrame(best_fit, columns=["DEAP e3", "DEAP e7", "DEAP e8", "EA e3", "EA e7", "EA e8"])
+    hof = pd.DataFrame(best_fit, columns=["CMA-ES e3", "CMA-ES e7", "CMA-ES e8", "SPO-EA e3", "SPO-EA e7", "SPO-EA e8"])
     hof.to_csv("hall_of_fame.csv")
-    
+
     hof.boxplot()
     plt.ylabel("Maximum fitness")
     plt.savefig("boxplot.png")
