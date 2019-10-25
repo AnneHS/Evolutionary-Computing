2cc48cd0e121a1a68d380ece0b3d3fcb6df128c6
RicardoKnauer <40441748+RicardoKnauer@users.noreply.github.com>
2019-10-25 21:51:23 +0200

    Update generalist_boxplots.py

--- generalist_boxplots.py
+++ generalist_boxplots.py
@@ -1,28 +1,23 @@
 import sys
-
 sys.path.insert(0, "evoman")
 
 import os
-
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-
 plt.style.use("ggplot")
 
 RUNS = 10
 
-
-
 if __name__ == "__main__":
 
+    # read CMA-ES and SPO-EA csv's
     cma = pd.read_csv('CMA_generalist_gain.csv', index_col=0, names=['cma'], header=0)
     ea = pd.read_csv('EA_generalist_gain.csv', index_col=0,  names=['ea'], header=0)
 
-
-
+    # prepare for plotting
     ea['enemy'] = list(range(1,9))*10
     ea['run'] = list(np.repeat(range(1,11), 8))
     cma['enemy'] = list(range(1, 9)) * 10
@@ -38,6 +33,6 @@ if __name__ == "__main__":
     ea_gain = ea.pivot(index='run', columns='enemy', values='ea')
     cma_gain = cma.pivot(index='run', columns='enemy', values='cma')
 
+    # boxplot
     ax = sns.boxplot(x="enemy", y="value", hue='method', data=both)
     ax.set_ylabel("Gain")
-
